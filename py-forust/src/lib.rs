853c323d681d5a7155c248114c1e7f602eb7d03d
jinlow <james.d.inlow@gmail.com>
2022-05-22 22:51:55 -0500

    starting to wrap with pyo3

--- py-forust/src/lib.rs
+++ py-forust/src/lib.rs
@@ -1,8 +1,70 @@
-use numpy::{PyReadonlyArray1};
 use forust::data::Matrix;
+use forust::gradientbooster::GradientBooster as CrateGradienBooster;
+use forust::objective;
+use forust::objective::ObjectiveType;
+use numpy::PyReadonlyArray1;
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+#[pyclass(subclass)]
+struct GradientBooster {
+    booster: CrateGradienBooster<f64>,
+}
+
+#[pymethods]
+impl GradientBooster {
+    #[new]
+    pub fn new(
+        objective_type: Option<&str>,
+        iterations: Option<usize>,
+        learning_rate: Option<f64>,
+        max_depth: Option<usize>,
+        max_leaves: Option<usize>,
+        l2: Option<f64>,
+        gamma: Option<f64>,
+        min_leaf_weight: Option<f64>,
+        base_score: Option<f64>,
+    ) -> Self {
+        let mut booster = CrateGradienBooster::<f64>::default();
+        match objective_type {
+            None => booster.objective_type = ObjectiveType::LogLoss,
+            Some(s) => {
+                if s == "LogLoss" {
+                    booster.objective_type = ObjectiveType::LogLoss
+                } else if s == "SquaredLoss" {
+                    booster.objective_type = ObjectiveType::SquaredLoss
+                } else {
+                    panic!("Not a valid objective type provided.")
+                }
+            }
+        }
+        if let Some(x) = iterations {
+            booster.iterations = x;
+        }
+        if let Some(x) = learning_rate {
+            booster.learning_rate = x;
+        }
+        if let Some(x) = max_depth {
+            booster.max_depth = x;
+        }
+        if let Some(x) = max_leaves {
+            booster.max_leaves = x;
+        }
+        if let Some(x) = l2 {
+            booster.l2 = x;
+        }
+        if let Some(x) = gamma {
+            booster.gamma = x;
+        }
+        if let Some(x) = min_leaf_weight {
+            booster.min_leaf_weight = x;
+        }
+        if let Some(x) = base_score {
+            booster.base_score = x;
+        }
+        GradientBooster { booster }
+    }
+}
+
 #[pyfunction]
 fn print_matrix(x: PyReadonlyArray1<f64>, rows: usize, cols: usize) -> PyResult<()> {
     let m = Matrix::new(x.as_slice()?, rows, cols);
