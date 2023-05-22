d367299027c9eb876105d7f1d5ba8737d7eb7dba
gravesee <eric.graves@lexisnexisrisk.com>
2023-05-22 08:11:39 -0500

    updates docs

--- py-forust/forust/__init__.py
+++ py-forust/forust/__init__.py
@@ -173,7 +173,7 @@ class GradientBooster:
                 Valid values are 0 to infinity. Defaults to 0.0.
             min_leaf_weight (float, optional): Minimum sum of the hessian values of the loss function
                 required to be in a node. Defaults to 1.0.
-            base_score (float, optional): The initial prediction value of the model. Defaults to 0.5.
+            base_score (float, optional): The initial prediction value of the model. Defaults to 0.5. If `None`, the `objective_type` will be used to calculate the initial base_score.
             nbins (int, optional): Number of bins to calculate to partition the data. Setting this to
                 a smaller number, will result in faster training time, while potentially sacrificing
                 accuracy. If there are more bins, than unique values in a column, all unique values
