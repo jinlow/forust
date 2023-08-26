ea2400b73ba29fc64bf9b3c5d9a9d9a18853226d
jinlow <james.inlow@lexisnexisrisk.com>
2023-08-26 10:46:08 -0500

    Run without optinoal deps

---
+++ scripts/remove-optional-deps.py
@@ -0,0 +1,13 @@
+# Building with polars is sooo slow.
+# It's only there for the example, so let's remove it
+# in the regular build process.
+# Requires toml package
+import toml
+
+ct = toml.load("cargo.toml")
+
+del ct["dev-dependencies"]
+del ct["bench"]
+
+with open("cargo.toml", "w") as file:
+    toml.dump(ct, file)
