3e77fba6dea3b04d36c3a9e07017a66c74ed9a24
jinlow <james.inlow@lexisnexisrisk.com>
2023-08-26 11:42:15 -0500

    Remove examples dir

--- scripts/remove-optional-deps.py
+++ scripts/remove-optional-deps.py
@@ -2,6 +2,8 @@
 # It's only there for the example, so let's remove it
 # in the regular build process.
 # Requires toml package
+import shutil
+
 import toml
 
 ct = toml.load("Cargo.toml")
@@ -11,3 +13,6 @@ del ct["bench"]
 
 with open("Cargo.toml", "w") as file:
     toml.dump(ct, file)
+
+# Also delete the rust example.
+shutil.rmtree("examples")
